7bbc3c12cc9cef350f88ca9fa31c1343660fd086
MRQ67 <kasaboody@gmail.com>
2025-06-11 22:26:23 +0300

    fix: Correct GitHub Actions workflow for release

--- cmd/version.go
+++ cmd/version.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const cliVersion = "0.1.4" // Define the current version
+const cliVersion = "0.1.6" // Define the current version
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
