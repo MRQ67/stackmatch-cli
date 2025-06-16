2060c8097cc1b6cf1bcfb552b2ae04daf093e940
MRQ67 <kasaboody@gmail.com>
2025-06-16 18:03:41 +0300

    Changed th Version

--- cmd/version.go
+++ cmd/version.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const cliVersion = "0.2.5" // Define the current version
+const cliVersion = "0.2.6" // Define the current version
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
